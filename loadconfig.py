2bd5d2ea051dfe68dd78bab23892f7b6567c4580
Shoroxi <40485962+Shoroxi@users.noreply.github.com>
2020-03-24 07:42:46 -0600

    Add files via upload

---
+++ loadconfig.py
@@ -0,0 +1,16 @@
+try:
+    from config.config import __token__, __prefix__, __kawaiichannel__, __botserverid__, __greetmsg__, __selfassignrole__, __cookieJar__, __discourseAPIKey__
+except ImportError:
+    #Heorku stuff
+    import os
+    __token__ = os.environ.get('DISCORD_TOKEN')
+    __prefix__ = os.environ.get('DISCORD_PREFIX')
+    __botserverid__ = int(os.environ.get('DISCORD_BOTSERVERID'))
+    __kawaiichannel__ = int(os.environ.get('DISCORD_KAWAIICHANNEL'))
+    __greetmsg__ = 659361592437112832
+    __selfassignrole__ = os.environ.get('DISCORD_SELFASSIGNROLE')
+    __cookieJar__ = os.environ.get('DISCORD_COOKIEJAR')
+    __discourseAPIKey__ = os.environ.get('DISCORD_DISCOURSEAPIKEY')
+
+from config.games import __games__, __gamesTimer__
+from config.cogs import __cogs__
