050e91e19c8cb52d0c536953e9afabc64aeadaf6
Shoroxi <40485962+Shoroxi@users.noreply.github.com>
2020-03-24 07:43:10 -0600

    Add files via upload

---
+++ config/cogs.py
@@ -0,0 +1,14 @@
+#Cogs to enable on Startup
+__cogs__ = [
+    'cogs.admin',
+    'cogs.anime',
+    'cogs.events',
+    'cogs.fun',
+    'cogs.help',
+    'cogs.mod',
+    'cogs.utility',
+    'cogs.math',
+    'cogs.music',
+    'cogs.voice',
+    #'cogs.nsfw',
+    ]
---
+++ config/config.py
@@ -0,0 +1,21 @@
+__token__ = 'NjA2MzY5NDY2NjYxNzMyMzUz.XnJ1Mg.cnYzImUgLenSIZNNyN-gI2BJ1zM'
+__prefix__ = '~'
+__botserverid__ = 589528167354597376 #Specifies the serverid from which the server-/modlog should be taken + some other nito features
+__kawaiichannel__ = 659231785757966337 #OPTIONAL specified a channel where the :kawaii commands gets this pinned messages
+__greetmsg__ = 659361592437112832 #HARDCODED Enable/Disable greetmsg at the default channel of my server
+__selfassignrole__ = 'Живой' #OPTIONAL set to a role to be self assign-able
+__discourseAPIKey__ = {'api_key': '9f0be26ea6a4f47dba6c627ee2f5aa139caa29f8b899bc96c3b7b94755a9363f', 'api_username': 'Eddy'} #OPTIONAL Needed for all discourse related commands
+__cookieJar__ = {'loginuuid': 'löaskdjfölkdfa65sdf46a5s4df56e1f23s1df32asd1f5ef1325s11bSJ9.ImRXcWNHeWI98sd5f4s65ef6s31h1fgjdfg6h54sdfasdfcvbcvb2465UrYlRORDYyTWREQThtYjBrcFEzd1wvbXZJRiswdlwvaWc5YkZSdDlMQUYzZEJIeE03SitsTFZhSFh5cGgrcHducXdzYjVMMTU5U0lGenNITitsYmdSSWIremlNU01UeVM5XC9ZaVpLSFBmaEkyU3NsZjQ1MVNDeHBvOGdaWWxGRlhmZ28raTlcL0RTYzlQaUJkc2N1alp4VW00dXJHMkd5UUttTnZQekZPa2Y1aE1qUjdHNVRwNkdyakhtNUtWc3VWYmUySVc0bkUyQ0pSWVMi.b6nMo5Q3hBMLauEsePPVNSdTJ8I5CqbZFDLrrln-oag', 'bbuserid': '5660970', 'bbpassword': '1753ec1889f70aa87845cc5bfd3b4409'} #OPTIONAL Needed for all elitepvpers related commands
+POST_ID = 672367971506585600
+ROLES = {
+    '🕵️‍♂️': 670261518771814401, #softach
+    '🎨': 670261516465078315, #3d artist
+    '💰': 670261506746613781, #buisnessman
+    '👌': 672421337628934183, #drochun
+    '👩‍🎤': 670264436895907861, #anime
+    '💀': 673437554485362690, #general
+}
+EXCROLES = (659359121228824586,)
+MAX_ROLES_PER_USER = 30
+channel_id = 672361190923436032
+message_id = 672367971506585600
