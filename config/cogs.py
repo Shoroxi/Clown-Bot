cc6cab651d5e744d91fe3b568bfcb077ee13639e
Shoroxi <40485962+Shoroxi@users.noreply.github.com>
2020-04-27 09:45:43 -0600

    Update cogs.py

--- config/cogs.py
+++ config/cogs.py
@@ -1,14 +1,14 @@
 #Cogs to enable on Startup
 __cogs__ = [
     'cogs.admin',
-    'cogs.anime',
+    #'cogs.anime',
     'cogs.events',
-    'cogs.fun',
+    #'cogs.fun',
     'cogs.help',
     'cogs.mod',
     'cogs.utility',
     'cogs.math',
-    'cogs.music',
-    'cogs.voice',
+    #'cogs.music',
+    #'cogs.voice',
     #'cogs.nsfw',
     ]
